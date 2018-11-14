f23657ed43b2493bf2d834bbee90375a469d33af
masterUNG <phrombutr@gmail.com>
2018-11-14 16:00:52 +0700

    Success

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'urlimage.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var titleString = 'Cache Image Form Internet';
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: titleString,
+      home: new Scaffold(
+        body: new UrlImage()
+      ),
+    );
+  }
+}
